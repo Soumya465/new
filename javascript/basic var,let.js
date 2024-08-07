e0998aa4ca0e0e6ee6ab44f4501c1968d4eca989
Soumya465 <pacharesoumya@gmail.com>
2024-08-07 23:12:27 +0530

    Add files via upload

---
+++ javascript/basic var,let.js
@@ -0,0 +1,33 @@
+
+
+//x = 5;
+//y = 6;
+//console.log(x+y)
+
+// Add var
+//var x = 5;
+//var y = 6;
+//console.log(x + y);
+// Sub let
+//let x = 5;
+//let y = 6;
+//console.log(x + y);
+// MULTIPLY CONST
+ //const x = 5;
+//const y = 6;
+//console.log(x * y);
+
+
+
+/* ----- let basic-------*/
+//let x = "apple";
+//let x = 0;
+//console.log(x)
+//Variables defined with let can not be redeclared.
+//Variables defined with var can be redeclared.
+//var x = "John Doe";
+//var x = 0;
+//console.log(x);
+//redeclare
+
+
---
+++ javascript/basic.js
@@ -0,0 +1,22 @@
+// Javascript is a programming language.
+//basic 
+//var a=5
+//console.log(a)
+//let a=5
+//console.log(a)
+//const a=5
+//console.log(a)
+
+//var a=5
+//var a=7// redeclare
+//console.log(a)
+
+ //let b=10
+ //let b=13
+ //console.log(b)
+ //SyntaxError: Identifier 'b' has already been declared
+ //const c=45
+  //c=56
+  //console.log(c)
+  //TypeError: Assignment to constant variable.
+
