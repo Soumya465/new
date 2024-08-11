ef5263317452224fcdcde336eb3176662f11d6cb
Soumya465 <pacharesoumya@gmail.com>
2024-08-11 22:00:49 +0530

    Add files via upload

---
+++ javascript/aRRAY/array.js
@@ -0,0 +1,14 @@
+var arr=[1,2,3,4,6,6,'string',true]
+//multiple value store in this array
+console.log(arr);
+// output:(8) [1, 2, 3, 4, 6, 6, 'string', true]
+//In &For use
+//for(let i=0; i<5;i++)
+    //for(let i in arr){
+   // console.log(i);
+//}
+//in->Index value print 0,1,2...
+//for(let i of arr){
+  // console.log(i);
+//}
+//of->Print the actual value of an array
