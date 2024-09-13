9619c1413bee692a516474f9baba04aab36c7ae9
Soumya465 <pacharesoumya@gmail.com>
2024-09-14 00:04:18 +0530

    Add files via upload

---
+++ javascript/lecture1.js
@@ -0,0 +1,37 @@
+//var x='HELLO WORLD'
+//console.log(x);
+//OUTPUT-HELLO WORLD
+
+//var x='HELLO WORLD'
+//var y='javascript'
+//console.log(x);
+//console.log(y);
+
+//OUTPUT -
+//HELLO WORLD
+ //javascript
+ 
+
+
+ //how to write variable name
+ var firstname="Arya"
+ console.log(firstname);
+
+ var first_name='savita'
+ console.log(first_name);
+
+ var firstName='savita'// CAMEL CASE
+console.log(firstName);
+
+var firstname99='savita'
+ console.log(firstname99);
+
+ //how to value assign
+  var first, second;
+  console.log(first);
+  output: undefined // not value assign
+   
+  var first, second ;
+  second=120;
+  console.log(second);
+  output: 120
