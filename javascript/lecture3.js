94016f00ed3cb8cb537bd8468d4862491f4ab96c
Soumya465 <pacharesoumya@gmail.com>
2024-09-20 21:48:42 +0530

    Add files via upload

---
+++ javascript/lecture3.js
@@ -0,0 +1,62 @@
+//DATA TYPES
+//String
+var x="helllo";
+console.log(x);
+//console.log("<br>");
+console.log(typeof x);
+//output:string type
+//helllo
+
+//number
+var y=89;
+
+console.log(y);
+
+console.log(typeof y);
+//output:number
+//89
+
+
+//boolean
+var y=true;
+console.log(y);
+
+console.log(typeof y);
+//output:boolean
+//true
+
+
+
+// array
+var y=["html","css","js"];
+console.log(y);
+
+console.log(typeof y);
+//output:(3) ['html', 'css', 'js']
+//object
+
+
+//object
+var y={first:"name",last:"name"};
+console.log(y);
+
+
+console.log(typeof y);
+//output:{first: 'name', last: 'name'}
+//object
+
+
+// Undefined
+var z;
+console.log(z);
+
+console.log(typeof z);
+//output:undefined
+
+
+// Undefined
+var z=null;
+console.log(z);
+
+console.log(typeof z);
+//output:undefined
