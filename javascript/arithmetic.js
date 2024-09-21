f0ec0751ad145893864d2489d56782f1c4deba3a
Soumya465 <pacharesoumya@gmail.com>
2024-09-21 22:32:13 +0530

    Add files via upload

---
+++ javascript/arithmetic.js
@@ -0,0 +1,51 @@
+var a=200;
+  var b=3;
+var  c=a+b;
+console.log(a+b);
+
+// Subtraction
+var a=200;
+  var b=300;
+var  c=a-b;
+console.log(a-b);
+
+
+// Multiply
+ var a=12;
+ var b=3;
+ var c=a*b;
+ console.log(a*b);
+
+
+ //Exponentation
+ var a=2;
+  var b=3;
+var  c=a**b;
+console.log(a**b);
+
+//Division
+var a=200;
+  var b=3;
+var  c=a/b;
+console.log(a/b);
+
+//Modulus
+var a=200;
+  var b=3;
+var  c=a%b;
+console.log(a%b);
+
+//Increment
+var a=200;
+  var b=3;
+  console.log(a+b);
+a++;
+console.log(a+b);
+
+
+//Decrement
+var a=200;
+  var b=3;
+  console.log(a+b)
+a--;
+console.log(a+b);
