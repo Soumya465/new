8ce9718265cd452ff333c304f3f9a4ca27e69fc8
Soumya465 <pacharesoumya@gmail.com>
2024-09-22 23:43:42 +0530

    Add files via upload

---
+++ javascript/assignmentoperator.js
@@ -0,0 +1,22 @@
+// Assign the value Add
+
+var a=10;
+b=3;
+a+=b;
+console.log(a);
+//output 13        
+//(a+b)=16 ans
+
+// Sub
+var a=10;
+b=3;
+a-=b;
+console.log(a);
+//output 7
+
+//exponential
+var a=10;
+b=3;
+a**=b;
+console.log(a);
+//Output 1000
