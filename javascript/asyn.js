397f8b41b771659094ed18c209cf05049b63a718
Soumya465 <pacharesoumya@gmail.com>
2024-09-29 22:45:18 +0530

    Add files via upload

---
+++ javascript/asyn.js
@@ -0,0 +1,29 @@
+// console.log('two');
+// setTimeout(()=>{
+// console.log('one');
+
+// },2000
+// )
+// console.log('three');
+
+// console.log('two');
+// setTimeout(()=>{
+//     console.log('ten');
+
+// },1000)
+ 
+// setTimeout(()=>{
+//     console.log('four');
+// },2000)
+// console.log('one');
+
+//------------------------------------Set interval------------------------------------------
+// setInterval(()=>{
+// console.log('heeee');
+// },2000)
+
+
+let id=setInterval(()=>{
+    console.log('heyy!!')
+},5000)
+//clearInterval(id)
