cd4e8392ce7a1f59a4585b14944e574d8339b00a
Soumya <pacharesoumya@gmail.com>
2024-10-04 23:33:16 +0530

    Add files via upload

---
+++ React js/Forms.jsx
@@ -0,0 +1,258 @@
+
+
+
+// import React from 'react'
+// import './App.css'
+// const App = () => {
+//   let arr=[1,2,3,4,56,7,8]
+//   return (
+//     <div  className=''>
+//        {
+//         arr.map((a)=>{
+//           return(<>
+//           <li>  a  </li>
+//           </>)
+//         })
+//        }
+//     </div>
+//   )
+// }
+
+// export default App
+
+// import React, { useState } from 'react'
+// const App = () => {
+//      let [count,SetCount]=    useState(0)
+//       let [color,SetColor]=    useState('red')
+//   // let count=0
+//   function fun1(){
+//     // console.log('hello');
+//     SetCount(count+1)      
+//     SetColor('green')
+//   }
+//   return (
+//     <div style={{backgroundColor:color,height:'100vh',width:"100vw"}}>
+//       <p>{count}</p>
+//      <button onClick={fun1}>click</button>
+//      <button onClick={fun1}> color green</button>
+//       {/* <Home   data={user}/> */}
+//     </div>
+//   )
+// }
+
+// export default App
+
+// import React from 'react'
+// import Clock from './Clock'
+// import Input from './Input'
+
+// const App = () => {
+//   return (
+//     <div>
+//       {/* <Clock/> */}
+//       <Input/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// import React from 'react'
+// import Todo from './todo'
+
+
+// const App = () => {
+//   return (
+//     <div>
+// <Todo/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// import React, { useEffect, useState } from 'react'
+
+// const App = () => {
+//   let [count,SetCount]=useState(0)
+//   let [city,SetCity]=useState('bhopal')
+//   let [api,SetApi]=useState([])
+//   useEffect(()=>{
+//     console.log('hellooo');
+//     fetch('https://dummyjson.com/recipes').then((res)=>{
+//       return  res.json()
+  
+//     }).then((data)=>{
+//       console.log(data.recipes,"hehe");
+//       SetApi(data.recipes)
+//       // console.log(api,"heheheheheheh");
+      
+
+      
+//     })
+//   },[])
+  
+//   function fun1(){
+//     SetCount(count+1)
+//   }
+//   function done(){
+//     SetCity('Delhi')
+//   }
+//   return (
+//     <div>
+//       <p> {count}</p>
+//       <button onClick={fun1}>click</button>
+
+//       <h3>{city}</h3>
+//       <button onClick={done}>change</button>
+
+// {
+//   api.map((a)=>{
+//     // console.log(a.name,"kya mil rha haiii ");
+//     return (<>
+//     <h2> {a.id}</h2>
+//     <p>{a.name  }</p>
+//     </>)
+    
+
+//   })
+// }
+
+
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// import React, { useEffect, useState } from 'react'
+
+// const App = () => {
+
+//   let [Api,SetApi]=useState([])
+
+//   useEffect( ()=>{
+
+//     fetch('https://dummyjson.com/recipes').then((res)=>{
+//       return res.json()
+
+//     }).then((data)=>{
+//       console.log(data.recipes,"heheheheh");
+//       SetApi(data.recipes)
+
+//     })
+//   },[])
+//   return (
+//     <div>
+//       {
+//         Api.map((val)=>{
+
+//           return (<>
+//           <h3> {val.id}</h3>
+//           <p>{val.name}</p>
+//           <img   height='100px'  src={val.image}/>
+//           </>)
+
+//         })
+//       }
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// import React from 'react'
+
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+// import SignUp from './SignUp'
+// import Login from './Login'
+// const App = () => {
+//   return (
+//     <div>
+//        <Router>   
+
+//       <Routes>
+//         <Route    path='/'  element={<SignUp/>} />
+//         <Route    path='/login'  element={<Login/>} />
+//       </Routes>
+//       </Router>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// import React from 'react'
+// import NavBar from './NavBar'
+// import C from './C'
+
+// const App = () => {
+//   return (
+//     <div>
+//       {/* <NavBar/>
+//       <C/> */}
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React, { useState } from 'react'
+
+const App = () => {
+  let [input,SetInput]=useState({
+    name:"",
+    email:"",
+    password:""
+
+  })
+
+
+  function fun1(e){
+    let {name,value}=   e.target
+    // console.log(e.target.value);
+    SetInput({...input,[name]:value})
+    // console.log(input,"dekhoo");
+    
+    
+  }
+
+  function done(e){
+    e.preventDefault()
+    console.log('heheheh',input);
+    
+  }
+  return (
+    <div>
+    {/* <h1> Form</h1> */}
+
+
+    <fieldset>   
+      <legend> Form</legend>
+    <form   onSubmit={done}> 
+      <input  type='text' name='name' value={input.name}   onChange={fun1}  placeholder='Enter your name'/>
+      <br></br>
+      <br></br>
+
+      <input  type='email' name='email' value={input.email}  onChange={fun1}  placeholder='Enter your email'/>
+      <br></br>
+      <br></br>
+
+      <input  type='password'  name='password'  value={input.password}  onChange={fun1} placeholder='Enter your passWord'/>
+<button  type='submit'> save</button>
+    </form>
+    </fieldset>
+    </div>
+  )
+}
+
+export default App
