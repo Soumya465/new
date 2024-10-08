d9990286874a2c809855181f4305b945aa948550
Soumya <pacharesoumya@gmail.com>
2024-10-08 23:43:35 +0530

    Add files via upload

---
+++ javascript/createfun.js
@@ -0,0 +1,20 @@
+//---------------------Application Create using function --------------------------------
+
+    setTimeout(function() {
+        console.log("photo select")
+    
+        setTimeout(function() { 
+            console.log("filter");
+            setTimeout(function() {
+                console.log("caption")
+            
+                setTimeout(function() { 
+                    console.log("post");
+        },3000);
+    },2000);
+},1000);
+},5000);
+
+
+   
+
